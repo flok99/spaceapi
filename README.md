9c66a1614f55ce0933b3a088a01b28cf0fd6e020
Folkert van Heusden <mail@vanheusden.com>
2020-01-18 20:40:47 +0100

    sorting

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Crontab:
 29 * * * *      /home/space/bin/update-spaceapi.py
 29 0 * * *      /home/space/bin/set-timezones.py
 */10 * * * *    /home/space/bin/get-spaceapi.py
-30 6 * * *      /home/space/bin/update-top.py > /home/space/www/open.inc.php
 
 
 
--- www/open.php
+++ www/open.php
@@ -10,12 +10,58 @@
 <div>
 <h1>SpaceAPI</h1>
 <div class="container">
-<p>On this page, you'll see for each space their global open/closed ratio percentage.</p>
-<? require('open.inc.php'); ?>
+
+<?php
+$mysqli = new mysqli('localhost', 'spaceapi', 'spaceapi', 'spaceapi');
+
+$sql = "SELECT `key`, name, url FROM spaces ORDER BY name";
+if (!$result = $mysqli->query($sql)) {
+	echo "Sorry, the website is experiencing problems.<br>";
+	echo "Errno: " . $mysqli->errno . "<br>\n";
+	echo "Error: " . $mysqli->error . "<br>\n";
+	exit;
+}
+
+$n = 0;
+while ($space = $result->fetch_assoc()) {
+	$key = $space['key'];
+	$name = $space['name'];
+	$url = $space['url'];
+
+	$sql2 = 'SELECT sum(open)/count(*) * 100 as open from data_' . $key;
+	if (!$result2 = $mysqli->query($sql2)) {
+		echo "Sorry, the website is experiencing problems.<br>";
+		echo "Errno: " . $mysqli->errno . "<br>\n";
+		echo "Error: " . $mysqli->error . "<br>\n";
+		exit;
+	}
+	$row2 = $result2->fetch_assoc();
+
+	$data[] = [ $name, $row2['open'] ];
+
+	$n++;
+}
+
+function sortByOrder($a, $b) {
+	    return $b[1] - $a[1];
+}
+
+usort($data, 'sortByOrder');
+?>
+<table>
+<tr><th>space</th><th>percentage open</th></tr>
+<?php
+for($i=0; $i<$n; $i++) {
+	if ($data[$i][1] >= 100 || $data[$i][1] <= 0.0)
+		continue;
+
+	?><tr><td><?php print $data[$i][0]; ?></td><td><?php print sprintf('%.2f', $data[$i][1]); ?>%</td></tr><?php
+}
+?>
+</table>
+
+</p>
 </div>
-<br>
-<p>Suggestions or whatever you feel like saying can be send via e-mail to: <a href="mailto:mail@vanheusden.com">mail@vanheusden.com</a></p>
-<br>
 
 <br>
 <br>
